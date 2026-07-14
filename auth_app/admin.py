7fce70e1a1b77e31d0a4928673f50db9259c735c
NicolausFeldtmann <n.feldtmann@yahoo.de>
2026-07-14 12:11:53 +0200

    update: adminpanel UserProfile updated

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from .models import UserProfile
 
-@admin.register(UserProfile)
-class UserProfile(admin.ModelAdmin):
-    list_display = ('__str__', "user", "email", "status")
+class CustomAdmin(admin.ModelAdmin):
+    list_filter = ["user", "email", "status"]
+    list_display = ["user", "email", "status"]
 
-    def get_user(self, obj):
-        return obj.user
-    get_user.short_description = 'User'
+admin.site.register(UserProfile, CustomAdmin)
